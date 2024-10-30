106d361bf445d6a49e07d52513a395bcaad3295a
Bruno Santos <ifgtmpw@gmail.com>
2024-10-30 11:52:54 +0000

    Update score_calculator_concept.py

--- Comparison_of_MSA_Softwares_Part2/score_calculator_concept.py
+++ Comparison_of_MSA_Softwares_Part2/score_calculator_concept.py
@@ -1,9 +1,9 @@
 def score_calculator(main_alignment_info, *other_alignement_info):
     tree_score = (tree_construction(main_alignment_info.output_file)*3)/max(tree_construction(main_alignment_info), tree_construction(other_alignment_info))
     memory_score = ((1/main_alignment_info.memory)*2)/min(1/main_alignment_info.memory, 1/other_alignment_info.memory)
-    speed_score = (1/main_alignment_info.speed)/min((1/main_alignment_info.speed), (1/other_alignment_info.speed))
+    exec_time_score = (1/main_alignment_info.exec_time)/min((1/main_alignment_info.exec_time), (1/other_alignment_info.exec_time))
     
-    return round(tree_score + memory_score + speed_score, 2)
+    return round(tree_score + memory_score + exec_time_score, 2)
 
 
     
