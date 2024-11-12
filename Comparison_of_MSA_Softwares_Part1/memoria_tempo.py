2e42e23a4a2759b8ba003aed3c1a70b5fc94f5cb
Alex112004 <alexandregodinhosoares@gmail.com>
2024-11-12 10:35:49 +0000

    codigo da função memoria e tempo

---
+++ Comparison_of_MSA_Softwares_Part1/memoria_tempo.py
@@ -0,0 +1,16 @@
+import time, sys, os, psutil
+
+def memoria_tempo(comando):
+    tempo_inicio=time.time()
+    processo = os.system(comando)
+    memoria_inicial=psutil.virtual_memory().used / (1024)
+    try:
+        while processo.poll():
+            memoria_total_c=psutil.virtual_memory().used / (1024)
+            memoria_total=max(memoria_total_c)
+    except Exception as erro_memoria:
+        print(f"Erro ao determinar a memoria {erro_memoria}")
+    memoria_utilizada=memoria_total-memoria_inicial
+    tempo_utilizado=time.time()-tempo_inicio
+
+    return  memoria_utilizada, tempo_utilizado
