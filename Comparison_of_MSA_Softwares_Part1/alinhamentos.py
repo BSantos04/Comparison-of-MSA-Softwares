ba76c9cdd3e03d007db003d23ad6b6951308ae12
Alex112004 <alexandregodinhosoares@gmail.com>
2024-11-15 17:54:55 +0000

    atualização das funções que correm os softwares de alinhamentos

--- Comparison_of_MSA_Softwares_Part1/alinhamentos.py
+++ Comparison_of_MSA_Softwares_Part1/alinhamentos.py
@@ -3,35 +3,21 @@ import os, sys
 fasta_input = sys.argv[1]
 
 def mafft(ficheiro_input, ficheiro_output):
-    comando = ["mafft --auto", ficheiro_input]
+    comando = f"mafft --auto {ficheiro_input} > {ficheiro_output}"
     os.system(comando)
     print(f"Alinhamento com o MAFFT está gravado como {ficheiro_output}")
 
 def muscle(ficheiro_input, ficheiro_output):
-    comando = ["muscle -in", ficheiro_input, "-out", ficheiro_output]
+    comando = f"muscle -align {ficheiro_input} -output {ficheiro_output}"
     os.system(comando)
     print(f"Alinhamento com o MUSCLE gravado como {ficheiro_output}")
 
 def tcoffee(ficheiro_input, ficheiro_output):
-    comando = ["t_coffee -in", ficheiro_input, "-output fasta_aln -outfile", ficheiro_output]
+    comando = f"t_coffee -in {ficheiro_input} -output fasta_aln -outfile {ficheiro_output}"
     os.system(comando)
     print(f"Alinhamento com o T-Coffee gravado como {ficheiro_output}")
 
 def clustalomega(ficheiro_input, ficheiro_output):
-    comando = ["clustalo -i", ficheiro_input, "-o", ficheiro_output, "--outfmt fasta"]
+    comando = f"clustalo -i {ficheiro_input} -o {ficheiro_output} --outfmt fasta"
     os.system(comando)
     print(f"Alinhamento com o Clustal Omega gravado como {ficheiro_output}")
-
-
-ficheiro_outputs = {
-    "mafft": "aligned_mafft.fasta",
-    "muscle": "aligned_muscle.fasta",
-    "tcoffee": "aligned_tcoffee.fasta",
-    "clustalomega": "aligned_clustalo.fasta",
-}
-
-
-mafft(fasta_input, ficheiro_outputs["mafft"])
-muscle(fasta_input, ficheiro_outputs["muscle"])
-tcoffee(fasta_input, ficheiro_outputs["tcoffee"])
-clustalomega(fasta_input, ficheiro_outputs["clustalo"])
