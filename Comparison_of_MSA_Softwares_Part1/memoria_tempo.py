6cc93ac737901e9bfa5bfd5d3148a7ea537a04c2
Alex112004 <alexandregodinhosoares@gmail.com>
2024-11-26 10:24:43 +0000

    Update memoria_tempo.py

--- Comparison_of_MSA_Softwares_Part1/memoria_tempo.py
+++ Comparison_of_MSA_Softwares_Part1/memoria_tempo.py
@@ -1,17 +1,18 @@
-import time, sys, os, psutil
+import time, os, psutil
+
 
 def memoria_tempo(comando):
-    tempo_inicio=time.time()
+    tempo_inicio = time.time()
     processo = os.system(comando)
-    memoria_inicial=psutil.virtual_memory().used / (1024)
-    memoria_total=memoria_inicial
+    memoria_inicial = psutil.virtual_memory().used / (1024)
+    memoria_total = memoria_inicial
     try:
         while processo.poll():
-            memoria_total_c=psutil.virtual_memory().used / (1024)
-            memoria_total=max(memoria_total_c)
+            memoria_total_c = psutil.virtual_memory().used / (1024)
+            memoria_total = max(memoria_total_c)
     except Exception as erro_memoria:
         print(f"Erro ao determinar a memoria {erro_memoria}")
-    memoria_utilizada=memoria_total-memoria_inicial
-    tempo_utilizado=time.time()-tempo_inicio
+    memoria_utilizada = memoria_total-memoria_inicial
+    tempo_utilizado = time.time()-tempo_inicio
 
-    return  memoria_utilizada, tempo_utilizado
+    return memoria_utilizada, tempo_utilizado
