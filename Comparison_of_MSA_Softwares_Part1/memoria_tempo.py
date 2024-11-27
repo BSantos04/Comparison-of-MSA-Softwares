1fc1087efdf07e25c0a05376ac0ecd4a37ff7bea
Bruno Santos <ifgtmpw@gmail.com>
2024-11-27 11:26:44 +0000

    Update memoria_tempo.py

--- Comparison_of_MSA_Softwares_Part1/memoria_tempo.py
+++ Comparison_of_MSA_Softwares_Part1/memoria_tempo.py
@@ -3,18 +3,30 @@ import os
 import psutil
 
 
-def memoria_tempo(comando):
-    tempo_inicio = time.time()
-    processo = os.system(comando)
-    memoria_inicial = psutil.virtual_memory().used / (1024)
-    memoria_total = memoria_inicial
-    try:
-        while processo.poll():
-            memoria_total_c = psutil.virtual_memory().used / (1024)
-            memoria_total = max(memoria_total_c)
-    except Exception as erro_memoria:
-        print(f"Erro ao determinar a memoria {erro_memoria}")
-    memoria_utilizada = memoria_total - memoria_inicial
-    tempo_utilizado = time.time() - tempo_inicio
+def get_memory_and_time(command):
+    """
+    Summary: 
+        This function gets the execution time and used memory of a process run by a command line.
+        
+    Parameters:
+        command: Input command line that will be parsed.
+        
+    Returns:
+        used_memory: Memory that was used during the process run by the command line.
+        exec_time: Time of execution of the process run by the command line.
 
-    return memoria_utilizada, tempo_utilizado
+    """
+    start_time = time.time()
+    process = os.system(command)
+    start_memory = psutil.virtual_memory().used / (1024)
+    total_memory = start_memory
+    try:
+        while process.poll():
+            interim_total_memory = psutil.virtual_memory().used / (1024)
+            total_memory = max(interim_total_memory)
+    except Exception as e:
+        print(f"An error has occured:\n{e}")
+    used_memory = total_memory - start_memory
+    exec_time = time.time() - start_time
+
+    return used_memory, exec_time
