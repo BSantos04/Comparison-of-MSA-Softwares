3751e5b0f58ac945268411e90491fb2302becc53
Alex112004 <alexandregodinhosoares@gmail.com>
2025-01-12 23:18:53 +0000

    Edit mostra_CPU.py

--- mostra_CPU.py
+++ mostra_CPU.py
@@ -1,29 +1,21 @@
+import subprocess
 import psutil
-import time
 import sys
 
-def mede_CPU_utilizado():
-    utilização_base=psutil.cpu_percent()    
-
-    tempo_0 = 1
-    tempo_c = 20
-    intervalo = 1
+def mede_CPU_utilizado(comando):
+    processo = subprocess.Popen(comando, shell=True)
+    utilização_base=psutil.cpu_percent()
+    em_utilização = 0
     em_utilização_t = 0
 
-    while tempo_0 > tempo_c:
-        em_utilização = psutil.cpu_percent()
+    try:
+        while processo.poll() is None:
+        em_utilização = psutil.cpu_percent(interval=1)
         em_utilização = max(em_utilização, em_utilização_t)
         em_utilização_t = em_utilização
 
-        time.sleep(intervalo)
-        tempo_0 += 1
-
+    except Exception as erro_utilização:
+        print(f"Erro ao determinar a CPU utilizada: {erro_utilização}")
     em_utilização = em_utilização_t - utilização_base
-    
-    print(f"A CPU utilizada no processo foi {em_utilização}%")
-    
-if __name__ == "__main__":
-    if len(sys.argv)!=1:
-        print("Usage: python3 {path/to/script.py}")
-    else:
-        mede_CPU_utilizado()
+
+    return  em_utilização
